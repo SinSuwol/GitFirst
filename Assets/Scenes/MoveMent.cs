131b0c0b0a8c433c3177ddf85d123ff25c543217
2018130 <35947698+2018130@users.noreply.github.com>
2022-06-21 17:36:27 +0900

    Git2

---
+++ Assets/Scenes/MoveMent.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveMent : MonoBehaviour
+{
+    const int a = 4;
+    //const  (»ó¼ö)
+    int b = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(a + b);
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
